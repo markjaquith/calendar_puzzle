7541c48ca2a881b2f2866b02543699c3e0e1e967
Mark Jaquith <mark@jaquith.me>
2025-01-12 10:39:18 -0500

    Use the quasi-standardized names of the pentominoes
    fixes #5

--- src/pieces.rs
+++ src/pieces.rs
@@ -2,87 +2,87 @@ use crate::piece::Piece;
 
 pub fn get_default_pieces() -> Vec<Piece> {
     vec![
-        // A A A A A
+        // I I I I I
         Piece::new(
-            'A',
+            'I',
             vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)],
             (255, 255, 255), // White foreground
             (64, 140, 86),   // Green background
         ),
-        // B B B B
-        // B
+        // L L L L
+        // L
         Piece::new(
-            'B',
+            'L',
             vec![(0, 0), (1, 0), (2, 0), (3, 0), (0, 1)],
             (255, 255, 255), // White foreground
             (0, 102, 204),   // Blue background
         ),
-        // C
-        // C C
-        // C C
+        // P
+        // P P
+        // P P
         Piece::new(
-            'C',
+            'P',
             vec![(0, 0), (0, 1), (1, 1), (0, 2), (1, 2)],
             (255, 255, 255), // White foreground
             (204, 102, 0),   // Orange background
         ),
-        // D D
-        // D
-        // D D
+        // U U
+        // U
+        // U U
         Piece::new(
-            'D',
+            'U',
             vec![(0, 0), (1, 0), (0, 1), (0, 2), (1, 2)],
             (0, 0, 0),       // Black foreground
             (239, 235, 231), // Light beige background
         ),
-        // E
-        // E E E
-        // E
+        // T
+        // T T T
+        // T
         Piece::new(
-            'E',
+            'T',
             vec![(0, 0), (0, 1), (0, 2), (1, 1), (2, 1)],
             (0, 0, 0),      // Black foreground
             (120, 81, 169), // Purple background
         ),
-        // F F F F
-        //   F
+        // Y Y Y Y
+        //   Y
         Piece::new(
-            'F',
+            'Y',
             vec![(0, 0), (1, 0), (2, 0), (3, 0), (1, 1)],
             (255, 255, 255), // White foreground
             (204, 102, 255), // Light purple background
         ),
-        // G
-        // G
-        // G G G
+        // V
+        // V
+        // V V V
         Piece::new(
-            'G',
+            'V',
             vec![(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
             (255, 255, 255), // White foreground
             (88, 28, 71),    // Deep magenta background
         ),
-        // H
-        // H H H
-        //     H
+        // Z
+        // Z Z Z
+        //     Z
         Piece::new(
-            'H',
+            'Z',
             vec![(0, 0), (1, 0), (1, 1), (1, 2), (2, 2)],
             (120, 60, 60),   // Dark reddish-brown foreground
             (255, 255, 255), // White background
         ),
-        //   I I I
-        // I I
+        //   N N N
+        // N N
         Piece::new(
-            'I',
+            'N',
             vec![(1, 0), (2, 0), (3, 0), (0, 1), (1, 1)],
             (255, 255, 255), // White foreground
             (102, 204, 153), // Soft teal background
         ),
-        //   J
-        // J J
-        //   J J
+        //   F
+        // F F
+        //   F F
         Piece::new(
-            'J',
+            'F',
             vec![(1, 0), (0, 1), (1, 1), (1, 2), (2, 2)],
             (0, 0, 0),       // Black foreground
             (255, 204, 102), // Light orange background
