b403bd5b045ab35846c424f8106e7d173f86e29b
Mark Jaquith <mark@jaquith.me>
2025-01-12 20:46:00 -0500

    Move Pieces functionality into a struct for organization

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use calendar::Day;
 use clap::Parser;
 use cli::{select_day, Args};
 use piece::{Piece, Rotation};
-use pieces::{get_corner_piece, get_default_pieces};
+use pieces::Pieces;
 
 use std::sync::atomic::AtomicBool;
 
@@ -27,36 +27,35 @@ fn main() {
         select_day()
     };
 
+    // Define the initial board.
+    let mut board = Board::new(BOARD_WIDTH, BOARD_HEIGHT, '·');
+
     // Create the three calendar pieces.
-    let month_piece = Piece::new('☻', vec![(0, 0)], (255, 255, 255), (0, 0, 0));
-    let day_piece = Piece::new('◉', vec![(0, 0)], (255, 255, 255), (0, 0, 0));
-    let weekday_piece = Piece::new('☼', vec![(0, 0)], (255, 255, 255), (0, 0, 0));
+    let month_piece = Pieces::get_month();
+    let day_piece = Pieces::get_day();
+    let weekday_piece = Pieces::get_weekday();
 
     // Create the corner piece.
-    let corner_piece = get_corner_piece();
-
-    // Define the initial board.
-    let mut initial_board = Board::new(BOARD_WIDTH, BOARD_HEIGHT, '·');
+    let corner_piece = Pieces::get_corner();
 
     // Place the calendar pieces on the board.
-    initial_board.place_piece(&month_piece, Rotation::Zero, day.month.to_coordinates());
-    initial_board.place_piece(&day_piece, Rotation::Zero, day.day.to_coordinates());
-    initial_board.place_piece(&weekday_piece, Rotation::Zero, day.weekday.to_coordinates());
+    board.place_piece(&month_piece, Rotation::Zero, day.month.to_coordinates());
+    board.place_piece(&day_piece, Rotation::Zero, day.day.to_coordinates());
+    board.place_piece(&weekday_piece, Rotation::Zero, day.weekday.to_coordinates());
 
-    // Corner piece
-    initial_board.place_piece(&corner_piece, Rotation::Zero, MISSING_CORNER_COORDINATES);
+    // Place the corner piece on the board.
+    board.place_piece(&corner_piece, Rotation::Zero, MISSING_CORNER_COORDINATES);
 
     println!("{}, {} {}", day.weekday, day.month, day.day);
-    initial_board.display();
+    board.display();
     println!();
 
     // Define the pieces to place
-    let owned_pieces = get_default_pieces();
-    let mut pieces: Vec<&Piece> = owned_pieces.iter().collect();
+    let default_pieces = Pieces::get_default();
 
     if args.show_pieces {
         println!("Pieces to place:");
-        for piece in &pieces {
+        for piece in &default_pieces {
             // Make an example board just big enough to display this piece.
             let mut example_board = Board::new(
                 piece.get_default_dimensions().0 as usize,
@@ -70,12 +69,12 @@ fn main() {
         }
     }
 
-    // Generate all valid boards that place all pieces
-    let final_boards = initial_board.find_boards_placing_all_pieces(
-        &mut pieces,
-        &AtomicBool::new(false),
-        args.all,
-    );
+    // Create a mutable list of references to the default pieces.
+    let mut pieces: Vec<&Piece> = default_pieces.iter().collect();
+
+    // Generate all valid boards that place all pieces.
+    let final_boards =
+        board.find_boards_placing_all_pieces(&mut pieces, &AtomicBool::new(false), args.all);
 
     if !args.all && final_boards.len() > 1 {
         println!(
--- src/pieces.rs
+++ src/pieces.rs
@@ -1,97 +1,113 @@
 use crate::piece::Piece;
 
-pub fn get_default_pieces() -> Vec<Piece> {
-    vec![
-        // F F
-        //   F F
-        //   F
-        Piece::new(
-            'F',
-            vec![(0, 0), (1, 0), (1, 1), (1, 2), (2, 1)],
-            (0, 0, 0),       // Black foreground
-            (255, 204, 102), // Light orange background
-        ),
-        // T T T
-        //   T
-        //   T
-        Piece::new(
-            'T',
-            vec![(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)],
-            (0, 0, 0),      // Black foreground
-            (120, 81, 169), // Purple background
-        ),
-        // U U
-        // U
-        // U U
-        Piece::new(
-            'U',
-            vec![(0, 0), (1, 0), (0, 1), (0, 2), (1, 2)],
-            (0, 0, 0),       // Black foreground
-            (239, 235, 231), // Light beige background
-        ),
-        // Z
-        // Z Z Z
-        //     Z
-        Piece::new(
-            'Z',
-            vec![(0, 0), (1, 0), (1, 1), (1, 2), (2, 2)],
-            (120, 60, 60),   // Dark reddish-brown foreground
-            (255, 255, 255), // White background
-        ),
-        // L L L L
-        // L
-        Piece::new(
-            'L',
-            vec![(0, 0), (1, 0), (2, 0), (3, 0), (0, 1)],
-            (255, 255, 255), // White foreground
-            (0, 102, 204),   // Blue background
-        ),
-        // V
-        // V
-        // V V V
-        Piece::new(
-            'V',
-            vec![(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
-            (255, 255, 255), // White foreground
-            (88, 28, 71),    // Deep magenta background
-        ),
-        // N
-        // N N
-        //   N
-        //   N
-        Piece::new(
-            'N',
-            vec![(0, 0), (0, 1), (1, 1), (1, 2), (1, 3)],
-            (255, 255, 255), // White foreground
-            (102, 204, 153), // Soft teal background
-        ),
-        // Y Y Y Y
-        //   Y
-        Piece::new(
-            'Y',
-            vec![(0, 0), (1, 0), (2, 0), (3, 0), (1, 1)],
-            (255, 255, 255), // White foreground
-            (204, 102, 255), // Light purple background
-        ),
-        // P
-        // P P
-        // P P
-        Piece::new(
-            'P',
-            vec![(0, 0), (0, 1), (1, 1), (0, 2), (1, 2)],
-            (255, 255, 255), // White foreground
-            (204, 102, 0),   // Orange background
-        ),
-        // I I I I I
-        Piece::new(
-            'I',
-            vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)],
-            (255, 255, 255), // White foreground
-            (64, 140, 86),   // Green background
-        ),
-    ]
-}
+pub struct Pieces;
 
-pub fn get_corner_piece() -> Piece {
-    Piece::new(' ', vec![(0, 0)], (255, 255, 255), (0, 0, 0))
+impl Pieces {
+    pub fn get_default() -> Vec<Piece> {
+        vec![
+            // F F
+            //   F F
+            //   F
+            Piece::new(
+                'F',
+                vec![(0, 0), (1, 0), (1, 1), (1, 2), (2, 1)],
+                (0, 0, 0),       // Black foreground
+                (255, 204, 102), // Light orange background
+            ),
+            // T T T
+            //   T
+            //   T
+            Piece::new(
+                'T',
+                vec![(0, 0), (1, 0), (2, 0), (1, 1), (1, 2)],
+                (0, 0, 0),      // Black foreground
+                (120, 81, 169), // Purple background
+            ),
+            // U U
+            // U
+            // U U
+            Piece::new(
+                'U',
+                vec![(0, 0), (1, 0), (0, 1), (0, 2), (1, 2)],
+                (0, 0, 0),       // Black foreground
+                (239, 235, 231), // Light beige background
+            ),
+            // Z
+            // Z Z Z
+            //     Z
+            Piece::new(
+                'Z',
+                vec![(0, 0), (1, 0), (1, 1), (1, 2), (2, 2)],
+                (120, 60, 60),   // Dark reddish-brown foreground
+                (255, 255, 255), // White background
+            ),
+            // L L L L
+            // L
+            Piece::new(
+                'L',
+                vec![(0, 0), (1, 0), (2, 0), (3, 0), (0, 1)],
+                (255, 255, 255), // White foreground
+                (0, 102, 204),   // Blue background
+            ),
+            // V
+            // V
+            // V V V
+            Piece::new(
+                'V',
+                vec![(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
+                (255, 255, 255), // White foreground
+                (88, 28, 71),    // Deep magenta background
+            ),
+            // N
+            // N N
+            //   N
+            //   N
+            Piece::new(
+                'N',
+                vec![(0, 0), (0, 1), (1, 1), (1, 2), (1, 3)],
+                (255, 255, 255), // White foreground
+                (102, 204, 153), // Soft teal background
+            ),
+            // Y Y Y Y
+            //   Y
+            Piece::new(
+                'Y',
+                vec![(0, 0), (1, 0), (2, 0), (3, 0), (1, 1)],
+                (255, 255, 255), // White foreground
+                (204, 102, 255), // Light purple background
+            ),
+            // P
+            // P P
+            // P P
+            Piece::new(
+                'P',
+                vec![(0, 0), (0, 1), (1, 1), (0, 2), (1, 2)],
+                (255, 255, 255), // White foreground
+                (204, 102, 0),   // Orange background
+            ),
+            // I I I I I
+            Piece::new(
+                'I',
+                vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)],
+                (255, 255, 255), // White foreground
+                (64, 140, 86),   // Green background
+            ),
+        ]
+    }
+
+    pub fn get_month() -> Piece {
+        Piece::new('☻', vec![(0, 0)], (255, 255, 255), (0, 0, 0))
+    }
+
+    pub fn get_day() -> Piece {
+        Piece::new('◉', vec![(0, 0)], (255, 255, 255), (0, 0, 0))
+    }
+
+    pub fn get_weekday() -> Piece {
+        Piece::new('☼', vec![(0, 0)], (255, 255, 255), (0, 0, 0))
+    }
+
+    pub fn get_corner() -> Piece {
+        Piece::new(' ', vec![(0, 0)], (255, 255, 255), (0, 0, 0))
+    }
 }
